226a2bacf82c27f3d889a1aaa3290b0731c16531
WorteXD <wortexd07@gmail.com>
2025-02-20 14:13:25 +0200

    Added welcome() function with ASCII art and colors

--- myShell.c
+++ myShell.c
@@ -66,33 +66,43 @@ int main(int argc, char const *argv[])
 }
 void welcome()
 {
-    char *logo[] = {"                     aRTI//BARK             ____\n",
-                    "             simpleSH//////////Shl         |B//S|", "    |\n",
-                    "          SH//////YS           shell//Sh   |A@@I|", "    | Welcome to simple shell\n",
-                    " Sim aSystemSH//Sh              sys//B     |R$$M|", "    | Version 1.0\n",
-                    " ArtABBBaraKk///Sh               sM//E     |A^^P|", "    |\n",
-                    "         pCCCCY//h          eSS@@ y//E     |K**L|", "    | https://github.com/BSharabi\n",
-                    "         SPPPP///a          pP///AC//E     |&&&E|", "    |\n",
-                    "              A//A            smP////S     |A**S|", "    | Have fun!\n",
-                    "              p///Ac            sE///a     |R##H|", "    |\n",
-                    "              P////YCpc           L//L     |T!!E|", "    | Wanna support simple shell?\n",
-                    "       scccccp///pSP///p          p//l     |I%%L|", "    |\n",
-                    "      sY/////////y  caa           S//h     |U--L|", "    |\n",
-                    "       shsSellH//Ya              pY/Sh     |MIGT|", "    |\n",
-                    "        sH/ShS////YCc          aC//Yp      |____|", "    |\n",
-                    "         Si  shlll//SHsimpleSH//LSs\n",
-                    "                  shell//////IPSHs\n",
-                    "                       artium\n", '\0'};
-    int i = 0;
+    printf("\033[1;30m================================================================================\033[0m\n");
 
-    do
-    {
-        printf("%s", logo[i++]);
-        // if (i % 2 != 0 || i > 26)
-        //     green();
-        // else
-        //     blue();
-    } while (logo[i]);
-    // reset();
-    puts("\n");
+    // Top section (blue)
+    printf("\033[1;34m                         __    _                                   \n");
+    printf("                    _wr\"\"        \"-q__                             \n");
+    printf("                 _dP                 9m_                          \n");
+    printf("               _#P                     9#_                        \n");
+    printf("              d#@                       9#m                       \n");
+    printf("             d##                         ###                      \n");
+    printf("            J###                         ###L                     \n");
+    printf("            {###K                       J###K                     \n");
+
+    // Middle section (white)
+    printf("\033[1;37m            ]####K      ___aaa___      J####F                     \n");
+    printf("        __gmM######_  w#P\"\"   \"\"9#m  _d#####Mmw__                \n");
+    printf("     _g##############mZ_         __g##############m_             \n");
+    printf("   _d####M@PPPP@@M#######Mmp gm#########@@PPP9@M####m_           \n");
+    printf("  a###\"\"          ,Z\"#####@\" '######\"\\g          \"\"M##m        \n");
+    printf(" J#@\"             0L  \"*##     ##@\"  J#              *#K        \n");
+    printf(" #\"               `#    \"_gmwgm_~    dF               `#_       \n");
+
+    // Bottom section (blue)
+    printf("\033[1;34m7F                 \"#_   ]#####F   _dK                 JE       \n");
+    printf("]                    *m__ ##### __g@\"                   F       \n");
+    printf("                       \"PJ#####LP\"                              \n");
+    printf(" `                       0######_                      '        \n");
+    printf("                       _0########_                              \n");
+    printf("     .               _d#####^#####m__              ,           \n");
+    printf("      \"*w_________am#####P\"   ~9#####mw_________w*\"             \n");
+    printf("          \"\"9@#####@M\"\"           \"\"P@#####@M\"\"                \n");
+
+    // Text box
+    printf("\033[1;33m  ╔═════════════════════════════╗\033[0m\n");
+    printf("\033[1;33m  ║\033[1;32m Made by: Nikita             \033[1;33m║\033[0m\n");
+    printf("\033[1;33m  ║\033[1;36m Welcome to My Shell         \033[1;33m║\033[0m\n");
+    printf("\033[1;33m  ║\033[1;35m Ariel University            \033[1;33m║\033[0m\n");
+    printf("\033[1;33m  ╚═════════════════════════════╝\033[0m\n");
+
+    printf("\033[1;30m================================================================================\033[0m\n");
 }
